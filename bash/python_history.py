3709558771c2fbbe3fdd62f385e93994061c71ab
Richard H <rafmagns-skepa-dreag@users.noreply.github.com>
2021-03-05 11:40:28 -0600

    Create python_history.py

---
+++ bash/python_history.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import os, readline, atexit
+
+python_history = os.path.join(os.environ['HOME'], '.python_history')
+try:
+  readline.read_history_file(python_history)
+  readline.parse_and_bind('tab: complete')
+  readline.set_history_length(5000)
+  atexit.register(readline.write_history_file, python_history)
+except IOError:
+  print('Failed to initialize python history')
+
+  del os, readline, atexit
