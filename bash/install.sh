a00dbd71bb97c00de118b9ee8572a2bba130c980
Richard H <rafmagns-skepa-dreag@users.noreply.github.com>
2021-03-24 13:13:03 -0500

    Update install.sh

--- bash/install.sh
+++ bash/install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-sudo yum install -y cmake zsh
+# sudo yum install -y cmake zsh
 mkdir $HOME/tools || echo tools already exists
 mkdir $HOME/.bin || echo .bin alrady exists
 mkdir -p $HOME/.config/nvim || echo .config already exists
+
+# cmake
+pushd $HOME/tools
+wget https://github.com/Kitware/CMake/releases/download/v3.20.0/cmake-3.20.0-linux-x86_64.tar.gz
+tar -zxvf cmake-3.20.0-linux-x86_64.tar.gz
+mv cmake-3.20.0-linux-x86_64/bin/* $HOME/.bin/
+popd
+export PATH=$HOME/.bin:$PATH
 
 # neovim
 git clone -b v0.4.4 https://github.com/neovim/neovim.git tools/neovim
@@ -13,6 +21,7 @@ popd
 
 # rust
 curl https://sh.rustup.rs -sSf | sh
+export PATH=$HOME/.cargo/bin:$PATH
 cargo install ripgrep exa python-launcher starship bat fd-find gitui
 
 # oh-my-zsh
@@ -23,6 +32,7 @@ curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poet
 
 # pyenv
 curl https://pyenv.run | bash
+export PATH=$HOME/.pyenv/bin:$PATH
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 pyenv install 3.8.8 &
@@ -37,7 +47,7 @@ $HOME/.fzf/install --completion --no-update-rc --key-bindings
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
 # curl init.vim, starship.toml, .tmux.conf, .ripgreprc
-# git clone https://github.com/rafmagns-skepa-dreag/stately-plump-buck $HOME/bootstrap
+git clone https://github.com/rafmagns-skepa-dreag/stately-plump-buck $HOME/bootstrap
 mv $HOME/bootstrap/bash/init.vim $HOME/.config/nvim/
 mv $HOME/bootstrap/bash/python_history.py $HOME/.pythonrc
 mv $HOME/bootstrap/bash/ripgreprc $HOME/.ripgreprc
