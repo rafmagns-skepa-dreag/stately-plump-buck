8b4ed1f7836f5930592a57b090b127db4936f0bf
Richard H <rafmagns-skepa-dreag@users.noreply.github.com>
2021-11-24 11:02:27 -0600

    Update plugins.lua

--- bash/plugins.lua
+++ bash/plugins.lua
@@ -4,8 +4,7 @@ local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
 return require('packer').startup(function()
@@ -20,4 +19,8 @@ return require('packer').startup(function()
   use 'iamcco/markdown-preview.nvim'
   use 'kosayoda/nvim-lightbulb'
   use 'nvim-lua/completion-nvim'
+    
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
