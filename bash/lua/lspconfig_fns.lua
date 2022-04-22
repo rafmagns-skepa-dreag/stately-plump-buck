5dfb6543f6687567034307a532af62f67e8f00fd
Richard H <rafmagns-skepa-dreag@users.noreply.github.com>
2022-04-22 17:32:13 -0500

    Update lspconfig_fns.lua

--- bash/lua/lspconfig_fns.lua
+++ bash/lua/lspconfig_fns.lua
@@ -1,11 +1,79 @@
 local M = {}
-local lspinstall = require'lspinstall'
-local lspconfig = require'lspconfig'
-local completion_callback = require'completion'.on_attach
+local lspinstall = require'nvim-lsp-installer'
 local util = require'lspconfig/util'
 
 local path = util.path
+vim.lsp.set_log_level("debug")
 
+local servers = {
+    "pyright",
+    "sumneko_lua"
+}
+
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+end
+
+
+-- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
+-- or if the server is already installed).
+lspinstall.on_server_ready(function(server)
+    local params = {}
+    params.on_attach = on_attach
+
+    if server.name == "pyright" then
+        params.before_init = function(_, config)
+            config.settings.python.pythonPath = M.get_python_path(config.root_dir)
+        end
+        params.settings = {python =
+            {venvPath = "/home/rhanson/projects/omcmono", venv=".venv",
+             verbose_output = true,
+             executionEnvironments = {}
+            }
+        }
+    end
+
+    if server.name == "sumneko_lua" then
+        params.settings = {
+            Lua = {
+                diagnostics = {globals = {'vim'}}
+            }
+        }
+    end
+
+    -- This setup() function will take the provided server configuration and decorate it with the necessary properties
+    -- before passing it onwards to lspconfig.
+    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+    server:setup(params)
+end)
 
 function M.get_python_path(workspace)
     -- Use activated venv
@@ -27,25 +95,13 @@ function M.get_python_path(workspace)
     return exepath('python3') or exepath('python') or 'python'
 end
 
-function M.setup_servers()
-    lspinstall.setup()
-    local servers = lspinstall.installed_servers()
-    for _, server in pairs(servers) do
-        if server == 'pyright' then
-            lspconfig[server].setup({
-                before_init = function(_, config)
-                    config.settings.python.pythonPath = M.get_python_path(config.root_dir)
-                end;
-                settings = {python = {venvPath = "/home/rhanson/omcmono", venv=".venv",
-                verbose_output = true,
-                executionEnvironments = {
-                }
 
-            }};
-                on_attach=completion_callback;
-            })
-        else
-            lspconfig[server].setup{on_attach=completion_callback}
+function M.setup_servers()
+    for _, name in pairs(servers) do
+        local server_is_found, server = lspinstall.get_server(name)
+        if server_is_found and not server:is_installed() then
+            print("Installing " .. name)
+            server:install()
         end
     end
 end
