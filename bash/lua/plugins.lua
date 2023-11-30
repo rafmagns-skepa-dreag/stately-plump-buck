0f0b8cdfee938fbaf134170cd5a7b8e218147792
Richard H <rafmagns-skepa-dreag@users.noreply.github.com>
2023-11-30 10:07:39 -0600

    Update plugins.lua

--- bash/lua/plugins.lua
+++ bash/lua/plugins.lua
@@ -24,118 +24,163 @@
 --end
 --vim.keymap.set("", "<f1>", toggle_profile)
 
-
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-        local packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-                install_path })
+    packer_bootstrap = fn.system({
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    })
 end
 
-return require('packer').startup({
-        function()
-                -- Packer can manage itself
-                use 'wbthomason/packer.nvim'
-                use {
-                        'utilyre/barbecue.nvim',
-                        requires = {
-                                "neovim/nvim-lspconfig",
-                                "SmiteshP/nvim-navic",
-                                "nvim-tree/nvim-web-devicons",
-                        },
-                        config = function()
-                                require("barbecue").setup()
-                        end,
-                }
+return require("packer").startup({
+    function()
+        -- Packer can manage itself
+        use("wbthomason/packer.nvim")
+        use({
+            "utilyre/barbecue.nvim",
+            requires = {
+                "neovim/nvim-lspconfig",
+                "SmiteshP/nvim-navic",
+                "nvim-tree/nvim-web-devicons",
+            },
+            config = function()
+                require("barbecue").setup()
+            end,
+        })
 
-                use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
+        use({ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" })
+        use({
+            "lewis6991/gitsigns.nvim",
+            config = function()
+                require("gitsigns").setup()
+            end,
+        })
 
-                -- TODO add my keymappings here
-                use {
-                        "folke/which-key.nvim",
-                        config = function()
-                                vim.o.timeout = true
-                                vim.o.timeoutlen = 300
-                                require("which-key").setup {}
-                        end
-                }
+        -- TODO add my keymappings here
+        use({
+            "folke/which-key.nvim",
+            config = function()
+                vim.o.timeout = true
+                vim.o.timeoutlen = 300
+                require("which-key").setup({})
+            end,
+        })
 
-                use {
-                        'williamboman/mason.nvim',
-                        -- cmd = "Mason",
-                        config = function()
-                                require("mason").setup({ PATH = "prepend" })
-                                local mr = require "mason-registry"
-                                local ensure_installed = { "stylua", "ruff-lsp" }
-                                for _, tool in ipairs(ensure_installed) do
-                                        local p = mr.get_package(tool)
-                                        if not p:is_installed() then
-                                                p:install()
-                                        end
-                                end
-                        end,
-                }
-                use {
-                        "williamboman/mason-lspconfig.nvim",
-                        requires = { "williamboman/mason.nvim" }
-                }
-
-                use {
-                        "jose-elias-alvarez/null-ls.nvim",
-                        --event = "BufReadPre",
-                        requires = { "nvim-lua/plenary.nvim", "williamboman/mason.nvim" },
-                        config = function()
-                                require("null-ls").setup {}
-                        end
-                }
-
-                use 'morhetz/gruvbox'
-                --use 'martinda/Jenkinsfile-vim-syntax'
-                --use 'junegunn/fzf'
-                use {
-                        'neovim/nvim-lspconfig',
-                        --event = 'BufReadPre',
-                        wants = { 'cmp-nvim-lsp', 'lsp_signature.nvim' },
-                        requires = { 'ray-x/lsp_signature.nvim' },
-                }
-                use 'iamcco/markdown-preview.nvim'
-                --use 'kosayoda/nvim-lightbulb'
-                use {
-                        "hrsh7th/nvim-cmp",
-                        --event = "InsertEnter",
-                        opt = false,
-                        wants = { "LuaSnip" },
-                        requires = {
-                                "hrsh7th/cmp-buffer",
-                                "hrsh7th/cmp-path",
-                                "hrsh7th/cmp-nvim-lua",
-                                "ray-x/cmp-treesitter",
-                                "hrsh7th/cmp-cmdline",
-                                "saadparwaiz1/cmp_luasnip",
-                                "hrsh7th/cmp-nvim-lsp",
-                                "hrsh7th/cmp-nvim-lsp-signature-help",
-                                -- "onsails/lspkind-nvim",
-                                -- "hrsh7th/cmp-calc",
-                                -- "f3fora/cmp-spell",
-                                -- "hrsh7th/cmp-emoji",
-                                {
-                                        "L3MON4D3/LuaSnip",
-                                        wants = "friendly-snippets",
-                                },
-                                "rafamadriz/friendly-snippets",
-                        },
-                        disable = false
-                }
-                --use 'stevearc/profile'
-                use { 'nvim-telescope/telescope.nvim', tag = '0.1.1', requires = { { 'nvim-lua/plenary.nvim' } } }
-
-
-                if packer_bootstrap then
-                        require('packer').sync()
+        use({
+            "williamboman/mason.nvim",
+            -- cmd = "Mason",
+            config = function()
+                require("mason").setup({ PATH = "prepend" })
+                local mr = require("mason-registry")
+                local ensure_installed = { "stylua", "ruff-lsp" }
+                for _, tool in ipairs(ensure_installed) do
+                    local p = mr.get_package(tool)
+                    if not p:is_installed() then
+                        p:install()
+                    end
                 end
-        end,
-        config = { transitive_opt = false, log_level = vim.log.levels.DEBUG }
+            end,
+        })
+        use({
+            "williamboman/mason-lspconfig.nvim",
+            requires = { "williamboman/mason.nvim" },
+        })
+
+        use("morhetz/gruvbox")
+        use({
+            "neovim/nvim-lspconfig",
+            --event = 'BufReadPre',
+            wants = { "cmp-nvim-lsp", "lsp_signature.nvim" },
+            requires = { "ray-x/lsp_signature.nvim" },
+        })
+        use("iamcco/markdown-preview.nvim")
+        --use 'kosayoda/nvim-lightbulb'
+
+        use({
+            "hrsh7th/nvim-cmp",
+            --event = "InsertEnter",
+            opt = false,
+            wants = { "LuaSnip" },
+            requires = {
+                "hrsh7th/cmp-buffer",
+                "hrsh7th/cmp-path",
+                "hrsh7th/cmp-nvim-lua",
+                "ray-x/cmp-treesitter",
+                "hrsh7th/cmp-cmdline",
+                "saadparwaiz1/cmp_luasnip",
+                "hrsh7th/cmp-nvim-lsp",
+                "hrsh7th/cmp-nvim-lsp-signature-help",
+                -- "onsails/lspkind-nvim",
+                -- "hrsh7th/cmp-calc",
+                -- "f3fora/cmp-spell",
+                -- "hrsh7th/cmp-emoji",
+                {
+                    "L3MON4D3/LuaSnip",
+                    wants = "friendly-snippets",
+                },
+                "rafamadriz/friendly-snippets",
+            },
+            disable = false,
+        })
+
+        --use 'stevearc/profile'
+        use({ "nvim-telescope/telescope.nvim", tag = "0.1.1", requires = { { "nvim-lua/plenary.nvim" } } })
+        use("mhartington/formatter.nvim")
+        use("mfussenegger/nvim-lint")
+
+        use({
+            "mickael-menu/zk-nvim",
+            config = function()
+                require("zk").setup()
+            end,
+        })
+        use("sainnhe/gruvbox-material")
+        use({
+            "nvim-lualine/lualine.nvim",
+            requires = { "nvim-tree/nvim-web-devicons", opt = true },
+        })
+
+        use({
+            "nvim-treesitter/nvim-treesitter",
+            run = ":TSUpdate",
+            config = function()
+                require("nvim-treesitter.configs").setup({
+                    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+                    ensure_installed = { "python", "javascript", "typescript", "lua", "csv", "psv", "json", "yaml" },
+                    auto_install = true,
+                })
+            end,
+        })
+
+        use({
+            "yorickpeterse/nvim-pqf",
+            config = function()
+                require("pqf").setup()
+            end,
+        })
+        use({
+            "kevinhwang91/nvim-ufo",
+            requires = "kevinhwang91/promise-async",
+            config = function()
+                require("ufo").setup({
+                    provider_selector = function(bufnr, filetype, buftype)
+                        return { "treesitter", "indent" }
+                    end,
+                })
+            end,
+        })
+
+        if packer_bootstrap then
+            require("packer").sync()
+        end
+    end,
+    config = { transitive_opt = false, log_level = vim.log.levels.INFO },
 })
