b530bd09c97d50ffaa475959ccb7993f2b165192
Richard H <rafmagns-skepa-dreag@users.noreply.github.com>
2025-02-13 16:04:14 -0600

    Update init.lua

--- bash/init.lua
+++ bash/init.lua
@@ -1,7 +1,9 @@
-require "plugins"
-require "autocmd"
-require "keymaps"
-require "options"
-require "lspconfig_completion"
+require("plugins")
+--require "plugins_new"
+require("autocmd")
+require("keymaps")
+require("options")
+require("lspconfig_completion")
 
+-- vim.lsp.set_log_level("debug")
 vim.api.nvim_set_option("clipboard", "unnamed")
