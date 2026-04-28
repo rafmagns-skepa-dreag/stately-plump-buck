72d8b05a9797cef67f2a580efc271a092757ee89
Richard H <rafmagns-skepa-dreag@users.noreply.github.com>
2026-04-28 13:32:50 -0500

    Update autocmds.lua

--- bash/lazyvim/lua/config/autocmds.lua
+++ bash/lazyvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "python" },
+	callback = function(event)
+		vim.opt.tabstop = 2
+		vim.opt.softtabstop = 2
+		vim.opt.shiftwidth = 2
+	end,
+})
