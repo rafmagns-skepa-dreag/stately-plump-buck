e5b2e149b4f9c42090f58cb73861377b600ddd39
Richard H <rafmagns-skepa-dreag@users.noreply.github.com>
2026-07-29 21:27:13 -0500

    Updates (#8)
    * Create starship-claude.sh
    
    * Update starship.toml
    
    * Create richad.lua
    
    * Update lazy-lock.json
    
    * Update lazyvim.json
    
    * Update autocmds.lua
    
    * Update lazy.lua
    
    * Update options.lua
    
    * Update richad.lua

--- bash/lazyvim/lua/config/options.lua
+++ bash/lazyvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local indent = 2
+vim.opt.mouse = nil
+vim.opt.softtabstop = indent
+vim.opt.tabstop = indent
+vim.opt.relativenumber = false
+
+vim.g.lazyvim_python_lsp = "ruff"
+vim.g.lazyvim_python_ruff = "ruff"
+vim.g.copilot_node_command = vim.fn.expand("$HOME") .. ".asdf/shims/node"
